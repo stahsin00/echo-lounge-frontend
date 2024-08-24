bfeac240a24689134d438cfaff35ad3f26c7609d
stahsin00 <lamisa1998@hotmail.com>
2024-08-24 03:06:53 -0700

    settings menu

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,8 +1,35 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { FaGlassMartiniAlt, FaCog, FaBookOpen, FaSignOutAlt, FaItunesNote, FaVolumeDown, FaCaretLeft, FaCaretRight, FaForward } from 'react-icons/fa';
 import './Game.css'
 
 function Game() {
-  const [count, setCount] = useState(0)
+    const [ muteMusic, setMuteMusic ] = useState(false);
+    const [ music, setMusic ] = useState(50);
+
+    const [ muteVolume, setMuteVolume ] = useState(false);
+    const [ volume, setVolume ] = useState(50);
+
+    const [ speed, setSpeed ] = useState(0);
+
+    const speedOptions = [1, 2, 4]
+
+    const handleMusicChange = (e) => {
+        setMuteMusic(false);
+        setMusic(e.target.value);
+      };
+    
+    const handleVolumeChange = (e) => {
+        setMuteVolume(false);
+        setVolume(e.target.value);
+      };
+
+    const handleSpeedIncrease = () => {
+        setSpeed((speed + 1) % speedOptions.length);
+      };
+    
+    const handleSpeedDecrease = () => {
+        setSpeed((speed - 1 + speedOptions.length) % speedOptions.length);
+      };
 
   return (
     <>
@@ -13,10 +40,46 @@ function Game() {
             </div>
             <div className='work-area'>
                 <div className="menu">
-                    Menu
+                    <button className='main-button'><FaGlassMartiniAlt className='button-icon' />Make Drink</button>
+                    <div className='sub-menu'>
+                        <button><FaBookOpen className='button-icon' />Recipes</button>
+                        <button><FaCog className='button-icon' />Settings</button>
+                        <button id='exit-button'><FaSignOutAlt className='button-icon' />Exit</button>
+                    </div>
                 </div>
                 <div className="drink">
-                    Drink
+                    <div className='setting'>
+                        <FaItunesNote className='setting-icon hover-cursor' onClick={() => setMuteMusic(!muteMusic)}/>
+                        <input
+                            type="range"
+                            min="0"
+                            max="100"
+                            value={muteMusic? 0 : music}
+                            onChange={handleMusicChange}
+                            className="slider"
+                        />
+                        <span>{muteMusic? 0 : music}</span>
+                    </div>
+                    <div className='setting'>
+                        <FaVolumeDown className='setting-icon hover-cursor' onClick={() => setMuteVolume(!muteVolume)} />
+                        <input
+                            type="range"
+                            min="0"
+                            max="100"
+                            value={muteVolume ? 0 : volume}
+                            onChange={handleVolumeChange}
+                            className="slider"
+                        />
+                        <span>{muteVolume ? 0 : volume}</span>
+                    </div>
+                    <div className='setting'>
+                        <FaForward className='setting-icon' />
+                        <span className='setting-span'>
+                            <FaCaretLeft className='hover-cursor' onClick={handleSpeedDecrease}/>
+                            {speedOptions[speed]}x
+                            <FaCaretRight className='hover-cursor' onClick={handleSpeedIncrease}/>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -25,7 +88,7 @@ function Game() {
             <div className="dialog">
                 <p><span>Customer :</span> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 <svg 
-                    className="triangle" 
+                    className="triangle hover-cursor" 
                     xmlns="http://www.w3.org/2000/svg" 
                     viewBox="0 0 100 100"
                 >

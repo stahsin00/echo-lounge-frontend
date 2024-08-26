124f62926387792a3024b28997e72ae83e3a262e
stahsin00 <lamisa1998@hotmail.com>
2024-08-26 00:06:33 -0700

    make drinks

--- src/components/Ingredient.jsx
+++ src/components/Ingredient.jsx
@@ -4,10 +4,10 @@ import { FaPlus, FaMinus } from 'react-icons/fa';
 function Ingredient(props) {
 
   return (
-    <div className={`ingredient ${props.color}`}>
-        <FaMinus className='hover-cursor'/>
+    <div className={`ingredient ${props.ingredient}`}>
+        <FaMinus className='hover-cursor' onClick={() => props.handleRemoveIngredient(props.ingredient)}/>
         {props.name}
-        <FaPlus className='hover-cursor'/>
+        <FaPlus className='hover-cursor' onClick={() => props.handleAddIngredient(props.ingredient)}/>
     </div>
   )
 }
